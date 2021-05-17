0dec7f336b5ddff8e977338b5e195bc77df70eea
Pranay-kumar-123 <vorugantipranay@gmail.com>
2021-05-17 08:09:06 +0530

    todo api

--- server.js
+++ server.js
@@ -1,9 +1,68 @@
 const express = require('express');
+const shortid = require("shortid");
 
 const app = express();
 app.use(express.static("frontend"));
 
+//T0O MAKE IT API SERVER
+ app.use(express.urlencoded({extended: true}));
+ app.use(express.json());
+// app.user(function(req, res, next){
+//     // all requests will go through it
+// })
+// app.use('/api/special', function(req, res, next){
+//     // this is only for apis whose url starts with /api/special
+// })
+let todo = [];
+app.get("/api/todos", (req, res) => {
+    res.status(200).json({
+        todo: todo
+    });
+})
+app.get("/api/todos/:todoid", (req, res) => {
+    let id = req.params.todoid;
+    let singletodo = todo.filter(data => (data.id == id))[0];
+    res.status(200).json({
+        singleitem: todo
+    })
 
+})
+app.post("/api/todos", (req, res) => {
+    let data = req.body;
+    let id = shortid.generate();
+    data.id = id;
+    todo.push(data);
+    res.status(200).json({
+        status: "added",
+        data: data
+    })
+})
+app.put("/api/todos/:todoid", (req, res) => {
+    let id = req.params.todoid;
+    let data = req.body
+    let idx = todo.findIndex((obj => obj.id == id));
+    console.log(req.body)
+    if (data.t == "toggle")
+        todo[idx].isActive = !todo[idx].isActive;
+    else
+        todo[idx].title = data.title;
+    console.log(todo[idx]);
+
+    res.status(200).json({
+        status: "updated",
+        data: todo[idx]
+    })
+})
+app.delete("/api/todos/:todoid", (req, res) => {
+    let id = req.params.todoid;
+    let idx = todo.findIndex((obj => obj.id == id));
+    let itemdata = todo[idx];
+    todo.splice(idx, 1);
+    res.status(200).json({
+        status: "deleted",
+        data: itemdata
+    })
+})
 app.get("/resume", function(req, res){
     res.sendFile(__dirname + "/frontend/html/resume.html");
 })
@@ -12,6 +71,15 @@ app.get("/color", function(req, res){
 })
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/frontend/html/google.html");
+    
+})
+app.get("/todoapi", function(req, res){
+    res.sendFile(__dirname + "/frontend/html/todoapi.html");
+    
+})
+app.get("/login1", function(req, res){
+    res.sendFile(__dirname + "/frontend/html/login1.html");
+    
 })
 app.get("/apple", function(req, res){
     res.sendFile(__dirname + "/frontend/html/apple.html");
@@ -32,7 +100,7 @@ app.get("/todo", function(req, res){
     res.sendFile(__dirname + "/frontend/html/todo.html");
 })
 // Heroku will automatically set an environment variable called PORT
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 // Start the server
 app.listen(PORT, function(){
